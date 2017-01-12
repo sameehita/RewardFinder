b79a46ff1bbe231fd48c61122de36ff75ee58596
caseychien <case.chien@gmail.com>
2017-01-12 14:24:22 -0500

    Added view controllers

--- RewardFinder/RewardFinder/ViewController.swift
+++ RewardFinder/RewardFinder/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Hello")
+    }
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstNameTextField.text = "Casey"
     }
 
     override func didReceiveMemoryWarning() {
