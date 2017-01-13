3b26a9f17ad8ebb01aae713681063ddf7cc269e8
sameehitamohan <SameeMohan@Sameehitas-MacBook-Air.local>
2017-01-12 23:15:15 -0500

    Merge remote-tracking branch 'caseychien/master'
    # Conflicts:
    #	RewardFinder/RewardFinder/Base.lproj/Main.storyboard

--- RewardFinder/RewardFinder/ResultViewController.swift
+++ RewardFinder/RewardFinder/ResultViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ResultViewController: UIViewController {
     var category: PurchaseCategory!
+    var data: Data!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        // Do any additional setup after loading the view.
+        
+        var myDictionary: [String: String]
+        myDictionary = ["Jessica": "Sloan"]
+        print(myDictionary)
+        
+        let path = Bundle.main.path(forResource: "creditCards", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        data = try? Data(contentsOf: url)
+        let creditCardData = parseJSON(for: data)
+        let bestCard = findBestReward(in: creditCardData)
+        
+    }
+    
+    func parseJSON(for data: Data!) -> [String: Any?]? {
+        let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+        if let dictionary = json as? [String: Any?] {
+            return dictionary
+        }
+        return nil
+    }
+    
+    func findBestReward(in creditCardData: [String:Any]?) -> String? {
+        return nil
     }
     
     override func didReceiveMemoryWarning() {
