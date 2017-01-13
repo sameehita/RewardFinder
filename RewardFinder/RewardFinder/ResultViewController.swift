e96bb8753fdd63696d07587ee71216c9f920af5b
Omar Baradei <omarbaradei21@gmail.com>
2017-01-12 19:51:56 -0500

    Fixed it all

--- RewardFinder/RewardFinder/ResultViewController.swift
+++ RewardFinder/RewardFinder/ResultViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ResultViewController: UIViewController {
     var category: PurchaseCategory!
+    var data: Data!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var myDictionary: [String: String]
+        myDictionary = ["Jessica": "Sloan"]
+        print(myDictionary)
+        
+        let path = Bundle.main.path(forResource: "creditCards", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        data = try? Data(contentsOf: url)
+        
+        let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+        
+        print(JSONSerialization.isValidJSONObject(json))
+        
+        if let dictionary = json as? [String: Any?] {
+            let cards = dictionary["cards"] as! [String: Any?]
+            print(cards.keys.count)
+        }
                 
         // Do any additional setup after loading the view.
     }
